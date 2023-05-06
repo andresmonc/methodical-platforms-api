80f44189468fd012f86d8b8bbbf0234bfaed91f4
andresmonc <jaimeamonc@gmail.com>
2023-05-06 10:41:24 -0500

    todo was resolved

--- src/main/java/com/methodicalplatforms/rentforecast/market/MarketRentForecastService.java
+++ src/main/java/com/methodicalplatforms/rentforecast/market/MarketRentForecastService.java
@@ -28,7 +28,7 @@ public class MarketRentForecastService {
         if (forecastedActualRent.compareTo(BigDecimal.ZERO) != 0 && excessRateAdjustmentRate.compareTo(BigDecimal.ZERO) != 0) {
             BigDecimal lossToLeasePercent = forecastedActualRent.subtract(forecastedMarketRent).divide(forecastedActualRent, HALF_EVEN);
             // If loss to lease percent exceed set market rent to actual rent, we're now the market leader
-            if (lossToLeasePercent.compareTo(excessRateAdjustmentRate) == 1) { // todo: need to figure out compare to
+            if (lossToLeasePercent.compareTo(excessRateAdjustmentRate) == 1) {
                 forecastedMarketRent = BigDecimal.ZERO.add(forecastedActualRent);
             }
         }
