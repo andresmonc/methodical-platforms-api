20ecde5d4a33bb5b1f7dfa1a0f13bff7620012e3
andresmonc <jaimeamonc@gmail.com>
2023-06-06 11:06:43 -0500

    bug fix

--- src/main/java/com/methodicalplatforms/rentforecast/market/MarketRentForecastService.java
+++ src/main/java/com/methodicalplatforms/rentforecast/market/MarketRentForecastService.java
@@ -17,7 +17,7 @@ public class MarketRentForecastService {
         BigDecimal forecastedMarketRent = priorMarketRent.multiply(marketEscalationRate);
 
         // avoid division by zero;
-        if (forecastedActualRent.compareTo(BigDecimal.ZERO) != 0 && excessRateAdjustmentRate.compareTo(BigDecimal.ZERO) != 0) {
+        if (forecastedActualRent.compareTo(BigDecimal.ZERO) != 0 && excessRateAdjustmentRate != null && excessRateAdjustmentRate.compareTo(BigDecimal.ZERO) != 0) {
             BigDecimal lossToLeasePercent = forecastedActualRent.subtract(forecastedMarketRent).divide(forecastedActualRent, HALF_EVEN);
             // If loss to lease percent exceed set market rent to actual rent, we're now the market leader
             if (lossToLeasePercent.compareTo(excessRateAdjustmentRate) > 0) {
