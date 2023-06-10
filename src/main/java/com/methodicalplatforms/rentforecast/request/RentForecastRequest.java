7aafd32b407d7eb3dc5375fc09d353996bccd1a6
andresmonc <jaimeamonc@gmail.com>
2023-06-10 15:10:09 -0500

    add closing date to request

--- src/main/java/com/methodicalplatforms/rentforecast/request/RentForecastRequest.java
+++ src/main/java/com/methodicalplatforms/rentforecast/request/RentForecastRequest.java
@@ -3,6 +3,7 @@ package com.methodicalplatforms.rentforecast.request;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -11,4 +12,5 @@ public class RentForecastRequest {
     @Builder.Default
     private RentForecastOptions options = new RentForecastOptions();
     private List<UnitTypeForecast> unitTypeForecastList;
+    private LocalDate closingDate;
 }
