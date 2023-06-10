031bcc9656063517e05156361d571ff32f538e8f
andresmonc <jaimeamonc@gmail.com>
2023-06-10 18:38:12 -0500

    lombok

--- src/main/java/com/methodicalplatforms/rentforecast/response/RentForecastMonth.java
+++ src/main/java/com/methodicalplatforms/rentforecast/response/RentForecastMonth.java
@@ -1,13 +1,17 @@
 package com.methodicalplatforms.rentforecast.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RentForecastMonth {
     private int year;
